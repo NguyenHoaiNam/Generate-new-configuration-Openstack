d4984c9494e2ee1b25b6408813635232d5802ae3
Dai Dang Van <daidv@vn.fujitsu.com>
2017-07-05 12:13:10 +0700

    Add tox

---
+++ setup.py
@@ -0,0 +1,17 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+import setuptools
+
+setuptools.setup(
+    name='jor',
+    version=0.1,
+    description='',
+    license='MIT',
+    keywords='',
+    url='https://github.com/NguyenHoaiNam/Jump-Over-Release',
+    install_requires=[''],
+    packages=[],
+)
