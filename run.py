d1ce94ea7b15f03b72acf5e7a4eae2beab692ad9
Nam Nguyen Hoai <namnh@vn.fujitsu.com>
2017-07-13 10:28:25 +0700

    finish step 3

---
+++ run.py
@@ -0,0 +1,14 @@
+# Author: Nam Nguyen Hoai
+# Author: Dai Dang Van
+
+from jor.getconf import oldconf
+from jor.configmapping import create_new_config
+
+
+if __name__ == '__main__':
+    CONF = oldconf.get_conf()
+    create_new_config.make_global_option('barbican')
+    create_new_config.change_old_config_to_new(create_new_config.NAME_NEW_FILE,
+                                               CONF)
+    create_new_config.move_option_changed(create_new_config.NAME_NEW_FILE,
+                                          CONF)
