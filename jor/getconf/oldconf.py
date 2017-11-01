72768aa9ae79ba70e38eb4dc15fa3da3d977b1f8
Dai Dang Van <daidv@vn.fujitsu.com>
2017-11-01 10:17:48 +0700

    Update log

--- jor/getconf/oldconf.py
+++ jor/getconf/oldconf.py
@@ -6,8 +6,11 @@ import importlib
 from oslo_config import cfg
 from oslo_config import generator as gn
 
+from jor import utils
+
 __all__ = ['get_conf', 'get_config_file']
 
+LOG = utils.get_log(__name__)
 
 DYNAMIC_SECTION_PROJECTS = {
     'cinder': 'jor.getconf.dynamic_section.cinder'
@@ -15,6 +18,8 @@ DYNAMIC_SECTION_PROJECTS = {
 
 
 def get_conf(conf_file=None, config_file=None):
+    """Get CONF object for specific project.
+    """
     conf = cfg.ConfigOpts()
     gn.register_cli_opts(conf)
     oslo_args = ['--config-file', conf_file]
@@ -38,24 +43,31 @@ def get_conf(conf_file=None, config_file=None):
             list_opts.extend(namespace[1])
         new_conf.register_group(group)
         if k == 'DEFAULT':
+            LOG.info("Force register DEFAULT group to easier access "
+                    "configuration values in DEFAULT group.")
             try:
                 new_conf.register_opts(list_opts)
             except cfg.DuplicateOptError:
                 continue
+        LOG.info("Register group %s into new_conf.", group.name)
         new_conf.register_opts(list_opts, group=group)
     projects = []
     for namespace in all_namespaces:
         sp = namespace.split('.')
+        LOG.info("Get project name from namespaces.")
         if 'oslo' in namespace:
+            LOG.info("Support getting project name for Oslo projects.")
             projects.append(".".join(sp[:2]))
         else:
             projects.append(sp[0])
 
     for project in projects:
+        LOG.info("Autoload all dynamic section.")
         try:
             get_name_module = DYNAMIC_SECTION_PROJECTS[project]
         except KeyError:
-            # This project does not have dynamic section
+            LOG.info("%s project does not have dynamic section or "
+                    "not is not supported now.")
             continue
         dynamic = importlib.import_module(get_name_module)
         dynamic.register_dynamic_section(new_conf)
@@ -64,6 +76,8 @@ def get_conf(conf_file=None, config_file=None):
 
 
 def get_ne_default(conf=None):
+    """Get not equal default value option.
+    """
     ne_dict = []
     if isinstance(conf, cfg.ConfigOpts):
         for name, group in conf._groups.items():
@@ -74,6 +88,8 @@ def get_ne_default(conf=None):
 
 
 def get_config_file(conf=None):
+    """Parsing config file following ini format.
+    """
     _list = []
     if conf:
         section_options = conf._namespace._parsed[0]
