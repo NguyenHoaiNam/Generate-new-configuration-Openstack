d2c6f1e5405c9f7042ae5f2b81fed2d4c61cd6fb
Nam Nguyen Hoai <namnh@vn.fujitsu.com>
2017-11-01 14:16:43 +0700

    Update source-code

--- jor/getconf/oldconf.py
+++ jor/getconf/oldconf.py
@@ -48,8 +48,12 @@ def get_conf(conf_file=None, config_file=None):
             try:
                 new_conf.register_opts(list_opts)
             except cfg.DuplicateOptError:
+<<<<<<< HEAD
                 continue
         LOG.info("Register group %s into new_conf.", group.name)
+=======
+                pass
+>>>>>>> 90a4cce... Update source-code
         new_conf.register_opts(list_opts, group=group)
     projects = []
     for namespace in all_namespaces:
